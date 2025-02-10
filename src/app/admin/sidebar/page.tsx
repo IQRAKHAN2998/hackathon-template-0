5ddc0560d32a61879eae6f0d97d0497c0c2424df
IQRAKHAN2998 <iqrakhan2998@gmail.com>
2025-02-10 23:03:05 +0500

    functionality

--- src/app/admin/sidebar/page.tsx
+++ src/app/admin/sidebar/page.tsx
@@ -7,7 +7,7 @@ const SideBar = () => {
     <Link href="/admin/products" className="p-2 bg-gray-200 rounded-md text-center  text-black hover:bg-gray-300">
       Products Stock
     </Link>
-    <Link href="orders" className="p-2 bg-gray-200 rounded-md text-center text-black hover:bg-gray-300">
+    <Link href="/admin/orders" className="p-2 bg-gray-200 rounded-md text-center text-black hover:bg-gray-300">
       Orders
     </Link>
     <Link href="/admin/overviewpage" className="p-2 bg-gray-200 rounded-md text-center text-black hover:bg-gray-300">
